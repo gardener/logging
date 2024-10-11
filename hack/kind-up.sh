ecb11c6c765ff5fa43266731ec71ff79c096c70a
Niki Dokovski <nickytd@gmail.com>
2024-10-11 22:48:53 +0200

    chore: add license headers

--- hack/kind-up.sh
+++ hack/kind-up.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
 
 source $(dirname $0)/.includes.sh
 name="local"
