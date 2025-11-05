efdfd415a44a34805e56aba55c11ab0b854de19e
Niki Dokovski <nikolai.dokovski@sap.com>
2025-11-05 11:28:57 +0100

    performance-env: allow custom values.yaml without interfering with git repo status

--- example/performance-test/setup.sh
+++ example/performance-test/setup.sh
@@ -7,9 +7,14 @@ dir=$(dirname "$0")
 kubectl apply -f ${dir}/cluster/cluster-crd.yaml
 namespace=fluent-bit
 
+values_arg=""
+if [ -f "${dir}/values.yaml" ]; then
+  values_arg="--values=${dir}/values.yaml"
+fi
+
 helm upgrade logging \
   ${dir}/charts/fluent-bit-plugin \
-  --values=${dir}/values.yaml \
+  $values_arg \
   --install \
   --namespace $namespace \
   --create-namespace \
