f87b57272e0519c8250a547b9b5aa933787842f3
Niki Dokovski <nikolai.dokovski@sap.com>
2025-11-20 18:20:50 +0100

    logs:  use texh handler in debug level logging

--- pkg/log/logger.go
+++ pkg/log/logger.go
@@ -26,7 +26,13 @@ func NewLoggerWithHandler(level string, output *os.File) logr.Logger {
 		AddSource: slogLevel == slog.LevelDebug,
 	}
 
-	handler := slog.NewJSONHandler(output, opts)
+	var handler slog.Handler
+	switch slogLevel {
+	case slog.LevelDebug:
+		handler = slog.NewTextHandler(output, opts)
+	default:
+		handler = slog.NewJSONHandler(output, opts)
+	}
 
 	return logr.FromSlogHandler(handler)
 }
